47f816c876f93d35932aa19cbf1c2ecd4a3da813
Israel <israelbekeletulu@gmail.com>
2025-02-18 13:25:42 +0300

    Task: upload

---
+++ src/index.js
@@ -0,0 +1,52 @@
+
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/users");
+        setUsers(response.data);
+      } catch (err) {
+        setError("Error fetching users");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h2>User List</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>{user.title} - {user.body}</li> // Assuming each user has 'id' and 'name'
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+root.render(
+ <UsersList/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
